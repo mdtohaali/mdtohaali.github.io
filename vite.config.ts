49acecc119a24072a7bff4201c391be05f5b8e10
মোঃ তোহা আলী <muhammadtohaali@proton.me>
2026-03-03 06:22:58 +0600

    Change base path in vite.config.ts
    Updated base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// Replace 'your-repo-name' with your GitHub repo
 export default defineConfig({
   plugins: [react()],
-  base: "/your-repo-name/",
-})
+  base: "/mdtohaali.github.io/", // GitHub repo name
+});
