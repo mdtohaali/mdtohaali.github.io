f714fb1bff0f260b8a24f271a7ced66ff376545f
মোঃ তোহা আলী <muhammadtohaali@proton.me>
2026-03-13 01:10:29 +0600

    Create graph-data.js

---
+++ header/profile/sidebar/graph/graph-data.js
@@ -0,0 +1,31 @@
+export const graphData = {
+    title: "জীবন মাইলফলক",
+    milestones: [
+        { 
+            id: "mother", 
+            label: "মা পাশে নেই",
+            priority: 1, // এটি ১ নম্বর কার্ড হিসেবে ফিক্সড থাকবে
+            startDate: "2020-05-07", 
+            color: "#636e72", 
+            maxDays: Infinity,
+            details: " ০৭ মে ২০২০<br>সময় স্রোতে আমার জন্নাত হারিয়ে ফেলেছি।"
+        },
+        { 
+            id: "mobile", 
+            label: "মোবাইল ক্রয়ের বয়স", 
+            startDate: "2021-09-03", 
+            color: "#00b894", 
+            maxDays: Infinity, 
+            details: "এটি আমার প্রথম স্মার্টফোন। ফোনটির নাম Tecno Spark 8 pro। মডেল: TECNO KG8।" 
+        },
+        { 
+            id: "cow", 
+            label: "গরু ক্রয়ের সময়", 
+            startDate: "2025-10-04", 
+            color: "White", 
+            maxDays: 730,
+            details: "এটি আমার উপার্জনে ক্রয় করা প্রথম গরু। যা SH Agro-এর একটি অংশ।" 
+        }
+    ],
+};
+
