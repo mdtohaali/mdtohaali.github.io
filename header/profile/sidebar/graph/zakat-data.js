a39e50d78ab97e029c799a576f08a924529a0b72
মোঃ তোহা আলী <muhammadtohaali@proton.me>
2026-03-13 01:13:02 +0600

    Create zakat-data.js

---
+++ header/profile/sidebar/graph/zakat-data.js
@@ -0,0 +1,12 @@
+export const zakatData = {
+    title: "জাকাত বর্ষপঞ্জি",
+    milestones: [
+        { 
+            id: "zakat_main", 
+            startDate: "1446-09-13", // জাকাত দেওয়ার তারিখ
+            color: "#27ae60", 
+            details: "حساب مقدار الثروة المطلوبة (سنة قمرية واحدة)." 
+        }
+    ]
+};
+ 
